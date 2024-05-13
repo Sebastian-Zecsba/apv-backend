64762e452e0ab7fc751a57945e48f326b8f414e5
Sebastian <sebastiancasallasarias2020@gmail.com>
2024-05-13 16:21:00 -0500

    cors

--- index.js
+++ index.js
@@ -11,18 +11,20 @@ conectarDB()
 
 const dominiosPermitidos = [process.env.FRONTEND_URL]
 
-const corsOptions = {
-    origin: function(origin, callback){
-        if(dominiosPermitidos.indexOf(origin) !== -1){
-            // El origen del Request esta permitido
-            callback(null, true)
-        }else{
-            callback(new Error('No permitido por CORS'))
-        }
-    }
-}
+// const corsOptions = {
+//     origin: function(origin, callback){
+//         if(dominiosPermitidos.indexOf(origin) !== -1){
+//             // El origen del Request esta permitido
+//             callback(null, true)
+//         }else{
+//             callback(new Error('No permitido por CORS'))
+//         }
+//     }
+// }
 
-app.use(cors(corsOptions))
+app.use(cors({
+    origin: 'https://apv-frontend-j7pm15ep0-sebastian-casallas-arias-projects.vercel.app'
+  }));
 
 app.use('/api/veterinarios', veterinariosRauters)
 app.use('/api/pacientes', pacienteRoutes)
