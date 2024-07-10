1d5f5f10cce3a03cd1f6218e1bf87350f3ade9da
Sebastian <sebastiancasallasarias2020@gmail.com>
2024-07-10 18:10:41 -0500

    MongoDB

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ app.use(express.json())
 
 conectarDB()
 
-// const dominiosPermitidos = [process.env.FRONTEND_URL]
+const dominiosPermitidos = [process.env.FRONTEND_URL]
 
-// const corsOptions = {
-//     origin: function(origin, callback){
-//         if(dominiosPermitidos.indexOf(origin) !== -1){
-//             // El origen del Request esta permitido
-//             callback(null, true)
-//         }else{
-//             callback(new Error('No permitido por CORS'))
-//         }
-//     }
-// }
+const corsOptions = {
+    origin: function(origin, callback){
+        if(dominiosPermitidos.indexOf(origin) !== -1){
+            // El origen del Request esta permitido
+            callback(null, true)
+        }else{
+            callback(new Error('No permitido por CORS'))
+        }
+    }
+}
 
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('/api/veterinarios', veterinariosRauters)
 app.use('/api/pacientes', pacienteRoutes)
