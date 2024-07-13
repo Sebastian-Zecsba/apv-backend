37758c1e58d0a30196b5cf42a378301405306839
Sebastian <sebastiancasallasarias2020@gmail.com>
2024-07-13 14:07:05 -0500

    Emails fixed

--- helpers/emailOlvidePassword.js
+++ helpers/emailOlvidePassword.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer'
 const emailOlvidePassword = async (datos) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+        service: process.env.EMAIL_PORT,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS
